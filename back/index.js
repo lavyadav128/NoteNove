ecdea5297edd51f5a4454c85a17398875cb7f3b7
lavyadav182 <lavkumaryadav824@gmail.com>
2025-05-25 08:43:35 +0530

    add

--- back/index.js
+++ back/index.js
@@ -13,7 +13,8 @@ const app = express();
 
 //  Middleware
 app.use(cors({
-  origin: ['https://notess-ei6q.onrender.com',
+  origin: [//'https://notess-ei6q.onrender.com',
+            'https://note-nove-k7zr.vercel.app',
             'http://localhost:3001'
   ], // Allow requests from your React frontend
   methods: ["GET", "POST", "PUT", "DELETE"],
