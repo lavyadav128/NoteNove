e75ac96885d24347bf947cb92102dc39fa2e10c5
lavyadav182 <lavkumaryadav824@gmail.com>
2025-05-26 20:03:48 +0530

    add

--- dash/src/environment.js
+++ dash/src/environment.js
@@ -1,12 +1,12 @@
-// const server ="http://localhost:3000"
-
-// export default server;
-
-
-let IS_PROD = true;
-const server = IS_PROD ?
-    "https://notes-szjj.onrender.com":
-    "http://localhost:3000"
-
+const server ="https://notes-szjj.onrender.com"
 
 export default server;
+
+
+// let IS_PROD = true;
+// const server = IS_PROD ?
+//     "https://notes-szjj.onrender.com":
+//     "http://localhost:3000"
+
+
+// export default server;
