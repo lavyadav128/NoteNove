1cf6c8fac80de625a6ec48ebec21c52098a3e446
lavyadav182 <lavkumaryadav824@gmail.com>
2025-06-15 14:36:54 +0530

    add

--- dash/src/components/Subjectpage.js
+++ dash/src/components/Subjectpage.js
@@ -1224,7 +1224,7 @@ const SubjectPage = () => {
               >
                 <Card
                   sx={{
-                    height: { xs: 100, sm: 140, md: 160 }, // 👈 Set smaller height on mobile (xs)
+                    height: { xs: 80, sm: 140, md: 160 }, // 👈 Set smaller height on mobile (xs)
                     minHeight: 120,
                     borderRadius: 3,
                     boxShadow:
